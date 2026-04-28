6c0d75fb09efe478a0544f5030f3398eb884303f
Celso Bitello <celsokaiiki@gmail.com>
2026-04-28 00:02:53 -0300

    C03 - Método Construtor e Encapsulamento

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,5 +1,32 @@
 class CalcController {
 
-    
+    constructor(){
+
+        this._displayCalc = "0"; //display da calculadora
+        this._dataAtual;
+
+    }
+ 
+    get displayCalc() {
+
+        return this._displayCalc;
+
+    }
+
+    set displayCalc(valor) {
+        this._displayCalc = valor; 
+    }
+
+    get dataAtual() {
+
+        return this._dataAtual;
+
+    }
+
+    set dataAtual(valor) {
+
+        this._dataAtual = valor;
+
+    }
 
 }
